6113a0568d83d1b43ceed0e1c13998bb761e698a
YesseJr <thonnieyesse@hotmail.com>
2022-01-10 08:45:26 +0300

    Selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+/*
+ * File: 1-insertion_sort.c
+ * Author: Waython Yesse
+ * Occupation: Software Engineering Student at ALX
+ * Address: waythonny@yahoo.com
+ * Year: 2022 January 10
+ * ** HAPPY NEW YEAR, HAPPY NEW CODE **
+ */
+
+#include "sort.h"
+
+/**
+ * selection_sort - sorts all selecty like
+ * @array: array to sort
+ * @size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int index = 0, min, temp;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				index = j;
+			}
+		}
+		if (min != array[i])
+		{
+			tmp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
+			print_array(array, size);
+		}
+	}
+}
