18c84e87a6acf87041e5e856051370bcf9255c05
YesseJr <thonnieyesse@hotmail.com>
2022-01-11 17:40:10 +0300

    Counting Sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,13 +8,11 @@
  */
 
 #include "sort.h"
-
 /**
  * counting_sort - counting sort algo
  * @array: array to sort
  * @size: size of array
  */
-
 void counting_sort(int *array, size_t size)
 {
 	size_t i;
@@ -23,7 +21,7 @@ void counting_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	k = array[0]; 
+	k = array[0]; /* find max num to malloc size of new array */
 	for (i = 1; i < size; i++)
 	{
 		if (array[i] > k)
@@ -32,19 +30,19 @@ void counting_sort(int *array, size_t size)
 	counts = malloc(sizeof(int) * (k + 1));
 	if (counts == NULL)
 		return;
-	for (j = 0; j < (k + 1); j++)
+	for (j = 0; j < (k + 1); j++) /* memset counts array to 0 */
 		counts[j] = 0;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++) /* input counts */
 	{
 		num = array[i];
 		counts[num] += 1;
 	}
-	for (j = 0; j < k; j++)
+	for (j = 0; j < k; j++) /* update counts array */
 	{
 		counts[j + 1] += counts[j];
 	}
 	print_array(counts, k + 1);
-	for (i = 0, j = 0; j < k; j++)
+	for (i = 0, j = 0; j < k; j++) /* replace array with sorted */
 	{
 		if ((j == 0) && counts[j] != 0)
 		{
